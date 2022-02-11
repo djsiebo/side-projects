19841e72d918a94a0f71d664fbd45652bbd9c476
David Siebold <djsiebo@gmail.com>
2022-02-11 08:52:26 -0500

    Moving project off laptop

--- Completed Side Projects/whiteboard011222/src/main/java/AnagramTest.java
+++ Completed Side Projects/whiteboard011222/src/main/java/AnagramTest.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AnagramTest {
 
     public static void main(String[] args) {
+        // Use scanner to request input from the user for 2 strings to compare
         Scanner userInput = new Scanner(System.in);
         System.out.print("What is the first string you want to compare?");
         String wordOne = userInput.nextLine();
@@ -18,9 +19,12 @@ public class AnagramTest {
         Map<String, Integer> mapOne = new HashMap<>();
         Map<String, Integer> mapTwo = new HashMap<>();
 
+        // Turn each string into an array of individual strings representing each alphanumeric character.
+        // Convert to lower case to make it easier to compare
         String[] arrayOne = wordOne.replaceAll("[^A-Za-z0-9]+", "").toLowerCase().split("");
         String[] arrayTwo = wordTwo.replaceAll("[^A-Za-z0-9]+", "").toLowerCase().split("");
 
+        // Iterate through the first array and load each value into a map, incrementing the value if the key already exists
         for (int i = 0; i < arrayOne.length; i++) {
             if (!mapOne.containsKey(arrayOne[i])) {
                 mapOne.put(arrayOne[i], 1);
@@ -29,6 +33,7 @@ public class AnagramTest {
             }
         }
 
+        // Iterate through the second array
         for (int i = 0; i < arrayTwo.length; i++) {
             if (!mapTwo.containsKey(arrayTwo[i])) {
                 mapTwo.put(arrayTwo[i], 1);
@@ -37,6 +42,7 @@ public class AnagramTest {
             }
         }
 
+        // Check to see if the maps are equal. Return true if they are equal, indicating they are anagrams of each other.
         if (mapOne.equals(mapTwo)) {
             System.out.println("\nThese strings are anagrams of each other.");
             return true;
